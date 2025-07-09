abeaab4de2ee9b967aff91afaf25c6947e90424a
Abdul21-dev <abdulrahman21062004@gmail.com>
2025-07-09 23:48:51 +0530

    Api testing

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const url = 'https://visual-crossing-weather.p.rapidapi.com/forecast?contentType=csv&unitGroup=us&aggregateHours=24&location=Washington%2CDC%2CUSA&shortColumnNames=false';
-const options = {
-	method: 'GET',
-	headers: {
-		'x-rapidapi-key': '74393a9dc6mshe60df839b6d708ap1940c5jsn2ff7d7389924',
-		'x-rapidapi-host': 'visual-crossing-weather.p.rapidapi.com'
-	}
-};
+// const url = 'https://open-weather13.p.rapidapi.com/Forecast?latitude=40.730610&longitude=-73.935242&lang=EN';
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'x-rapidapi-key': '74393a9dc6mshe60df839b6d708ap1940c5jsn2ff7d7389924',
+// 		'x-rapidapi-host': 'open-weather13.p.rapidapi.com'
+// 	}
+// };
 
-async function call() {
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}
-};
+// async function call() {
+// try {
+// 	const response = await fetch(url, options);
+// 	const result = await response.text();
+// 	console.log(result);
+// } catch (error) {
+// 	console.error(error);
+// }
+// };
